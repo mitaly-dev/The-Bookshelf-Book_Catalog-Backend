adf20e60fb3e2d9e9e000d7383062635b57209b3
mitaly-dev <mitalyy.dev@gmail.com>
2023-07-16 22:03:52 +0600

    featured route created

--- src/app/modules/books/book.route.ts
+++ src/app/modules/books/book.route.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get('/:id', BookCtrl.getSingleBook);
 router.get('/', BookCtrl.getAllBooks);
+router.get('/featuredBook', BookCtrl.getFeaturedBooks);
 router.post('/', BookCtrl.addNewBook);
 router.patch('/:id', BookCtrl.updateBook);
 router.delete('/:id', BookCtrl.deleteBook);
